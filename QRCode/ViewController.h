8e81a7d4f01cd192be9f03272ae34cec3b9c305f
liangxiu.chen <liangxiu.chen@husor.com>
2016-07-22 09:14:34 +0800

    Initial Commit

---
+++ QRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCode
+//
+//  Created by liangxiu.chen on 16/7/22.
+//  Copyright © 2016年 liangxiu.chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
