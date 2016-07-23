f7e00ec21401075cc8e0fa531c7d278fdab3cbd9
liangxiu.chen <liangxiu.chen.cn@gmail.com>
2016-07-23 15:54:03 +0800

    组件基本结构完成

--- QRCode/AppDelegate.m
+++ QRCode/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    UINavigationController *rootNav = [UINavigationController new];
+    UIViewController *rootVC = ViewController.new;
+    [rootNav pushViewController:rootVC animated:NO];
+    self.window.rootViewController = rootNav;
     return YES;
 }
 
--- QRCode/ViewController.m
+++ QRCode/ViewController.m
@@ -7,21 +7,95 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
+<
+AVCaptureMetadataOutputObjectsDelegate
+>
 
+@property (nonatomic, strong) AVCaptureSession *qrCodeSession;
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *layer;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.extendedLayoutIncludesOpaqueBars = NO;
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"fdasf" style:UIBarButtonItemStyleDone target:self action:@selector(start)];
+    self.title = @"二维码扫描";
+    [self start];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (BOOL)authoriseDevice
+{
+    AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+    switch (status) {
+        case AVAuthorizationStatusNotDetermined:
+        {
+            __block BOOL result;
+            [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+                result = granted;
+                !granted ?: NSLog(@"fadsf");
+            }];
+            return result;
+            break;
+        }
+        case AVAuthorizationStatusAuthorized:
+        {
+            return YES;
+            break;
+        }
+        default:
+        {
+            NSLog(@"fadsf");
+            return NO;
+            break;
+        };
+    }
+}
+
+- (void)start{
+    if (![self authoriseDevice]) {
+        return;
+    }
+    self.qrCodeSession = [AVCaptureSession new];
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    NSError *error = nil;
+    AVCaptureDeviceInput *deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
+    if (deviceInput && error == nil) {
+        [self.qrCodeSession addInput:deviceInput];
+        
+        AVCaptureMetadataOutput *metadataOutput = [[AVCaptureMetadataOutput alloc] init];
+        [metadataOutput setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+        [self.qrCodeSession addOutput:metadataOutput];
+        metadataOutput.metadataObjectTypes = @[AVMetadataObjectTypeQRCode];
+        
+        self.layer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.qrCodeSession];
+        self.layer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+        self.layer.frame = self.view.bounds;
+        [self.view.layer insertSublayer:self.layer atIndex:0];
+    
+        [self.qrCodeSession startRunning];
+        
+    }
+    
+}
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
+{
+    if (metadataObjects.count == 0) {
+        return;
+    }
+    AVMetadataObject *transformedObj = [self.layer transformedMetadataObjectForMetadataObject:metadataObjects.lastObject];
+    if ([transformedObj isKindOfClass:[AVMetadataMachineReadableCodeObject class]]) {
+        AVMetadataMachineReadableCodeObject *machineCode = (AVMetadataMachineReadableCodeObject *)transformedObj;
+        if (machineCode.type == AVMetadataObjectTypeQRCode) {
+            NSLog(@"%@",NSStringFromCGRect(machineCode.bounds));
+        }
+    }
 }
 
 @end
